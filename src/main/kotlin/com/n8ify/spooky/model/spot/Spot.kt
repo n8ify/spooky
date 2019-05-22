287b71f4d21440f2e0e44554d7d966db99e7ec92
Nattawut P <nata8ify@gmail.com>
2019-05-22 11:39:12 +0700

    Expose Timestamp Deserialization (2)

--- src/main/kotlin/com/n8ify/spooky/model/spot/Spot.kt
+++ src/main/kotlin/com/n8ify/spooky/model/spot/Spot.kt
@@ -3,4 +3,4 @@ package com.n8ify.spooky.model.spot
 import com.google.gson.annotations.Expose
 import java.sql.Timestamp
 
-data class Spot(val id : Int, val tale : String, val description : String?, val remark : String?, val status : Int, val latitude : Float, val longitude : Float, @Expose(deserialize = false, serialize = true) val createTimestamp: Timestamp)
+data class Spot(val id : Int, val tale : String, val description : String?, val remark : String?, val status : Int, val latitude : Float, val longitude : Float, @Expose(deserialize = true, serialize = false) val createTimestamp: Timestamp)
