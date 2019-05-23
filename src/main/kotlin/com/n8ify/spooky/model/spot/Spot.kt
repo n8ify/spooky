317fe76646b83fe4231766d9201cac104f4a4cd0
Nattawut P <nata8ify@gmail.com>
2019-05-23 18:37:46 +0700

    *

--- src/main/kotlin/com/n8ify/spooky/model/spot/Spot.kt
+++ src/main/kotlin/com/n8ify/spooky/model/spot/Spot.kt
@@ -1,6 +1,7 @@
 package com.n8ify.spooky.model.spot
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.google.gson.annotations.Expose
 import java.sql.Timestamp
 
-data class Spot(val id : Int, val tale : String, val description : String?, val remark : String?, val status : Int, val latitude : Float, val longitude : Float, @Expose(serialize = true, deserialize = false) val createTimestamp: Timestamp? = null)
+data class Spot(val id : Int, val tale : String, val description : String?, val remark : String?, val status : Int, val latitude : Float, val longitude : Float, @JsonFormat(pattern = "E, d MMM yyyy HH:mm:ss a") val createTimestamp: Timestamp? = null)
