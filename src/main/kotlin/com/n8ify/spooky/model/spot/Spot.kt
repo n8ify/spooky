07f3ede0a5f5a9ecaf3dd16f15b1f422e7e24fca
Nattawut P <nata8ify@gmail.com>
2020-06-01 10:57:29 +0700

    defualt create timestamp

--- src/main/kotlin/com/n8ify/spooky/model/spot/Spot.kt
+++ src/main/kotlin/com/n8ify/spooky/model/spot/Spot.kt
@@ -3,5 +3,6 @@ package com.n8ify.spooky.model.spot
 import com.google.gson.annotations.Expose
 import org.springframework.format.annotation.DateTimeFormat
 import java.sql.Timestamp
+import java.time.LocalDateTime
 
-data class Spot(val id : Int, val tale : String, val description : String?, val remark : String?, val status : Int, val latitude : Float, val longitude : Float, @DateTimeFormat(pattern = "E d, MMM yyyy h:mm:ss a") val createTimestamp: Timestamp? = null)
+data class Spot(val id : Int, val tale : String, val description : String?, val remark : String?, val status : Int, val latitude : Float, val longitude : Float, @DateTimeFormat(pattern = "E d, MMM yyyy h:mm:ss a") val createTimestamp: Timestamp? = Timestamp.valueOf(LocalDateTime.now()))
